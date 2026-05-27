ade18020911d3c1cf9e8128ef1274b92941173a6
Lavish-Garg01 <garglavish01@gmail.com>
2026-05-28 02:06:50 +0530

    fixed backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://food-delivery-app-vs2w.onrender.com',
         changeOrigin: true,
       }
     }
